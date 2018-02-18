60c80fa2b3b27331965f724b3192ff579604760b
Lola Priego <md.priego@gmail.com>
2018-02-18 16:58:50 -0500

    Renamed topic 2 existing exercises.

--- topic2_lists/InsertNodeAtTail.java
+++ topic2_lists/InsertNodeAtTail.java
@@ -2,9 +2,10 @@
 
 // HackerRank documentation.
 /*
-  Insert Node at the end of a linked list 
+  Insert Node at the end of a linked list
   head pointer input could be NULL as well for empty list
-  Node is defined as 
+  Node is defined as
+  
   class Node {
      int data;
      Node next;
@@ -12,7 +13,7 @@
 */
 
 /**
-* Our solution. Please note that there could always be more than one. 
+* Our solution. Please note that there could always be more than one.
 *
 */
 Node Insert(Node head,int data) {
@@ -20,10 +21,10 @@ Node Insert(Node head,int data) {
     tail.data = data;
     if (head == null) {
         return tail;
-    } 
-    
+    }
+
     Node trackingNode = head;
-        
+
     while (trackingNode.next != null) {
         trackingNode = trackingNode.next;
     }
